ffd5aa9c03b0c13648b054be9ba8d32068a36830
백승현 <songh1000@gmail.com>
2022-02-22 17:23:22 +0900

    백승현 : 0222 숙제 커밋

---
+++ 0222Homework/0222Homework.cpp
@@ -0,0 +1,39 @@
+﻿// 0222Homework.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> Arr = std::vector<int>(5);
+
+    Arr[0] = 3;
+    Arr[1] = 2;
+    Arr[2] = 1;
+    Arr[3] = 4;
+    Arr[4] = 5;
+
+    int Cnt = Arr.size();
+    for (size_t i = 0; i < Cnt; i++)
+    {
+        for (size_t j = 0; j < Cnt - (i+1); j++)
+        {
+            int iValue = Arr[i];
+            int jValue = Arr[j+1];
+
+            if (Arr[j] > Arr[j+1])
+            {
+                int Value = Arr[j + 1];
+                Arr[j + 1] = Arr[j];
+                Arr[j] = Value;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < Cnt; i++)
+    {
+        std::cout << Arr[i] << std::endl;
+    }
+
+}
---
+++ 0222Homework/MyMap.h
@@ -0,0 +1,11 @@
+#pragma once
+
+template<typename DataType>
+class MyMap
+{
+private:
+	MapArr<DataType> ArrData_;
+	unsigned int Key_;
+
+public:
+};
